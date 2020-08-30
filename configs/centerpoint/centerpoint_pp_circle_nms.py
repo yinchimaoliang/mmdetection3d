26979b21d6f4ef09507a615290f3706d8838632a
liyinhao <liyinhao@sensetime.com>
2020-08-30 11:06:41 +0800

    Change schedule in config.

--- configs/centerpoint/centerpoint_pp_circle_nms.py
+++ configs/centerpoint/centerpoint_pp_circle_nms.py
@@ -162,13 +162,17 @@ optimizer = dict(type='AdamW', lr=0.001, weight_decay=0.01)
 # max_norm=10 is better for SECOND
 optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
 lr_config = dict(
-    policy='step',
-    warmup='linear',
-    warmup_iters=1000,
-    warmup_ratio=1.0 / 1000,
-    step=[16, 19, 20])
-momentum_config = None
+    policy='cyclic',
+    target_ratio=(10, 1e-4),
+    cyclic_times=1,
+    step_ratio_up=0.4,
+)
+momentum_config = dict(
+    policy='cyclic',
+    target_ratio=(0.85 / 0.95, 1),
+    cyclic_times=1,
+    step_ratio_up=0.4,
+)
+
 # runtime settings
 total_epochs = 20
-
-evaluation = dict(interval=24)
--- configs/centerpoint/centerpoint_pp_circle_nms_object_sample.py
+++ configs/centerpoint/centerpoint_pp_circle_nms_object_sample.py
@@ -160,15 +160,17 @@ data = dict(
 
 optimizer = dict(type='AdamW', lr=0.001, weight_decay=0.01)
 # max_norm=10 is better for SECOND
-optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
 lr_config = dict(
-    policy='step',
-    warmup='linear',
-    warmup_iters=1000,
-    warmup_ratio=1.0 / 1000,
-    step=[16, 19, 20])
-momentum_config = None
-# runtime settings
-total_epochs = 20
+    policy='cyclic',
+    target_ratio=(10, 1e-4),
+    cyclic_times=1,
+    step_ratio_up=0.4,
+)
+momentum_config = dict(
+    policy='cyclic',
+    target_ratio=(0.85 / 0.95, 1),
+    cyclic_times=1,
+    step_ratio_up=0.4,
+)
 
 evaluation = dict(interval=24)
