65147dace6177b20dd47be21a5855e366a3b47cc
liyinhao <liyinhao@sensetime.com>
2020-08-30 21:40:16 +0800

    Add double flip in train pipeline.

--- configs/centerpoint/centerpoint_pp_dcn_circle_nms.py
+++ configs/centerpoint/centerpoint_pp_dcn_circle_nms.py
@@ -41,7 +41,10 @@ train_pipeline = [
         rot_range=[-0.3925, 0.3925],
         scale_ratio_range=[0.95, 1.05],
         translation_std=[0, 0, 0]),
-    dict(type='RandomFlip3D', flip_ratio_bev_horizontal=0.5),
+    dict(
+        type='RandomFlip3D',
+        flip_ratio_bev_horizontal=0.5,
+        flip_ratio_bev_vertical=0.5),
     dict(type='PointsRangeFilter', point_cloud_range=point_cloud_range),
     dict(type='ObjectRangeFilter', point_cloud_range=point_cloud_range),
     dict(type='ObjectNameFilter', classes=class_names),
